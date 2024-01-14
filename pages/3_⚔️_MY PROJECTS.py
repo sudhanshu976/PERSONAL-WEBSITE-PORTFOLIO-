3a44dc9d8a546a1f3fd621c2a7ce1cb496f46d76
SUDHANSHU <gusainsudhanshu43@gmail.com>
2024-01-14 15:26:55 +0530

    final

--- pages/3_⚔️_MY PROJECTS.py
+++ pages/3_⚔️_MY PROJECTS.py
@@ -1,8 +1,9 @@
 import streamlit as st
+
+# Set page configuration
 st.set_page_config(
     page_title="PERSONAL WEBSITE"
 )
-import streamlit as st
 
 # Main content
 st.title("My Projects")
@@ -10,21 +11,21 @@ st.title("My Projects")
 # List of projects
 projects = [
     {"name": "ATM APP", "description": "Python and SQL Project", "link": "ATM-APP"},
-    {"name": "BLOG WEBSITE", "description": "Website purely made with CHATGPT ", "link": "https://sudhanshu976.github.io/"},
-    {"name": "POWER BI DASHBOARDS", "description": "Data Cleaning / Power BI Dashboards.", "link": "POWER-BI_PROJECTS"},
-    {"name": "ML-APP", "description": " ML-MODEL FLASK APP ", "link": "FOOTBALL_PREDICTION_ML_FLASK_APP"},
-    {"name": "MLOPS PROJECT 1 ( Regression )", "description": "MLOPS / Python / CI-CD Pipeline", "link": "MLOPS_PROJECT"},
-    {"name": "MLOPS PROJECT 2 ( Classification )", "description": "MLOPS / Python / Azure / Github Actions ", "link": "MUSHROOM_MLOPS"},
+    {"name": "BLOG WEBSITE", "description": "Website purely made with CHATGPT", "link": "https://sudhanshu976.github.io/"},
+    {"name": "POWER BI DASHBOARDS", "description": "Data Cleaning / Power BI Dashboards", "link": "POWER-BI_PROJECTS"},
+    {"name": "ML-APP", "description": "ML-MODEL FLASK APP", "link": "FOOTBALL_PREDICTION_ML_FLASK_APP"},
+    {"name": "MLOPS PROJECT 1 (Regression)", "description": "MLOPS / Python / CI-CD Pipeline", "link": "MLOPS_PROJECT"},
+    {"name": "MLOPS PROJECT 2 (Classification)", "description": "MLOPS / Python / Azure / Github Actions", "link": "MUSHROOM_MLOPS"},
     {"name": "NLP PROJECTS", "description": "4 NLP projects", "link": "NLP_PROJECTS"},
-    {"name": "POTATO DISEASE CLASSIFIER", "description":"CNN APP for Image Classification", "link": "POTATO_DISEASE_CLASSIFIER"},
+    {"name": "POTATO DISEASE CLASSIFIER", "description": "CNN APP for Image Classification", "link": "POTATO_DISEASE_CLASSIFIER"},
     {"name": "MALARIA DIAGNOSIS", "description": "Image Classification", "link": "MALARIA_DIAGNOSIS"},
     {"name": "HUMAN EMOTIONS DETECTOR", "description": "Image-Classification", "link": "HUMAN_EMOTIONS_DETECTOR"},
-    {"name": "CAR COUNTER APP", "description": "Object-Detection / Tracking / YOLO ", "link": "CAR_COUNTER_APP_(_CV-1_)"},
-    {"name": "FACE ATTENDANCE SYSTEM", "description": "OpenCV / Face-recognition library ", "link": "FACE_ATTENDANCE_SYSTEM_(_CV-2)"},
+    {"name": "CAR COUNTER APP", "description": "Object-Detection / Tracking / YOLO", "link": "CAR_COUNTER_APP_(_CV-1_)"},
+    {"name": "FACE ATTENDANCE SYSTEM", "description": "OpenCV / Face-recognition library", "link": "FACE_ATTENDANCE_SYSTEM_(_CV-2_)"},
     {"name": "REAL OR FAKE PERSON DETECTOR", "description": "OpenCV / Custom Model Object Detection / Model ZOO", "link": "REAL_OR_FAKE_PERSON_DETECTOR_(CV-3)"},
+    {"name": "INVOICE EXTRACTOR USING GEMINI AI", "description": "  Streamlit | Langchain | Geminni-Pro-Vision ", "link": "INVOICE_EXTRACTOR_GENAI"},
 ]
 
-# Display project list with links
-for project in projects:
-    st.write(f"**{project['name']}**: {project['description']} - [Link to Project]({project['link']})")
-
+# Display project list with numbers and links
+for index, project in enumerate(projects, start=1):
+    st.markdown(f"{index}. **{project['name']}**: {project['description']} - [Link to Project]({project['link']})")
