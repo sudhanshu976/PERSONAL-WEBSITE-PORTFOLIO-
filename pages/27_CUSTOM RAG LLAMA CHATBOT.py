18321d2d709fc7b82f92995aa420312393a4420a
SUDHANSHU <gusainsudhanshu43@gmail.com>
2024-04-11 17:21:28 +0530

    final

---
+++ pages/27_CUSTOM RAG LLAMA CHATBOT.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.set_page_config(
+    page_title="PERSONAL WEBSITE")
+
+if st.button("Go to Project"):
+    redirect_link = "https://github.com/sudhanshu976/END-TO-END-CUSTOM-RAG-LLAMA-2-CHATBOT"  # Replace with the URL you want to redirect to
+    st.markdown(f"[Click here to go to this project code]({redirect_link})")
+
+st.title(" CUSTOM LLM CHATBOT USING MEDITRON ")
+video_path = "videos/custom_chatbot.mp4"
+st.video(video_path)
